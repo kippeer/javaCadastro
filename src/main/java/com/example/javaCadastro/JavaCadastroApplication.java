e202bdc3048b627804aa718a3b405dc95688759b
Fabio <fabiot92@gmail.com>
2024-02-10 13:11:22 -0300

    init

---
+++ src/main/java/com/example/javaCadastro/JavaCadastroApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaCadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCadastroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javaCadastro/JavaCadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaCadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
