22221ac95f6e947a04322d6822bc7078f2626f73
Fabio <fabiot92@gmail.com>
2024-02-10 16:00:24 -0300

    Registration, app user

--- backend/src/main/java/com/example/backend/registration/RegistrationService.java
+++ backend/src/main/java/com/example/backend/registration/RegistrationService.java
@@ -1,7 +1,29 @@
 package com.example.backend.registration;
 
+import com.example.backend.appUser.AppUser;
+import com.example.backend.appUser.AppUserRole;
+import com.example.backend.appUser.AppUserService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class RegistrationService {
+    private final AppUserService appUserService;
+    private EmailValidator emailValidator;
     public String register(RegistrationRequest request) {
-        return "works";
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+        if (!isValidEmail){
+            throw new IllegalStateException("EMAIL_NOT_VALID");
+        }
+        return appUserService.singUpUser(
+                new AppUser(
+                        request.getFirstname(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        AppUserRole.USER
+                )
+        );
     }
 }
